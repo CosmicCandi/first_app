950a5256a14219ed1ee022c4d9abff074f29a8fd
CosmicCandi <kalea.wolff@gmail.com>
2017-02-22 16:04:42 -0500

    Initial commit that completes explorer mode.

--- first_app.rb
+++ first_app.rb
@@ -1,1 +1,30 @@
 require 'sinatra'
+
+get '/' do
+  'Hello World'
+end
+
+get '/not_found' do
+  status 404
+  "We were unable to find that page, sorry for any inconvenience."
+end
+
+get "/lipsum" do
+  status 200
+  "You almost had it. <br><br> Try: <br><ul> <li>/lipsum/bacon</li> <br> <li>/lipsum/pirate</li> <br> <li>/lipsum/sagan</li></ul>"
+end
+
+get "/:name" do
+  "Hi there, #{params['name'].capitalize}"
+end
+
+get '/lipsum/:name' do
+  case params[:name]
+  when "bacon"
+      p "bacon".upcase
+    when "pirate"
+      p "pirate"
+    when "sagan"
+      p "<h1>sagan</h1>"
+  end
+end
--- tests.rb
+++ tests.rb
@@ -28,8 +28,26 @@ class FirstAppTest < Test::Unit::TestCase
   end
 
   def test_lipsums_page
-    # TODO: Fill me in to check each lipsum you support.
+    get "/lipsum"
+    assert_equal 200, last_response.status
+    assert_equal "You almost had it. <br><br> Try: <br><ul> <li>/lipsum/bacon</li> <br> <li>/lipsum/pirate</li> <br> <li>/lipsum/sagan</li></ul>", last_response.body
   end
 
+  def test_bacon_lipsum
+    get "/lipsum/bacon"
+    assert last_response.ok?
+    assert_equal "bacon".upcase, last_response.body
+  end
+
+  def test_pirate_lipsum
+    get "/lipsum/pirate"
+    assert_equal 200, last_response.status
+    assert_equal "pirate", last_response.body
+  end
+
+  def test_sagan_lipsum
+    get "/lipsum/sagan"
+    assert_equal "<h1>sagan</h1>", last_response.body
+  end
 
 end
